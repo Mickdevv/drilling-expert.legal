0e16948592e8e74d4147f945f886189a414da420
Mickdevv <michael@devereux.fm>
2023-10-12 17:34:07 +0200

    Basic website skeleton. Navbar centered and functional

--- Drilling_expert_legal/main/urls.py
+++ Drilling_expert_legal/main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.homePage, name="homePage"),
+    path("homePage/", views.homePage, name="homePage"),
+    path("eLearning/", views.eLearning, name="eLearning"),
+    path("Expert/", views.Expert, name="Expert"),
+    path("Investigator/", views.Investigator, name="Investigator"),
+    path("Mediator/", views.Mediator, name="Mediator"),
+    path("Arbitrator/", views.Arbitrator, name="Arbitrator"),
 ]
--- Drilling_expert_legal/main/views.py
+++ Drilling_expert_legal/main/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-def homePage(response):
-    return HttpResponse("Test")
+def homePage(request):
+    template = loader.get_template('homePage.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def Arbitrator(request):
+    template = loader.get_template('Arbitrator.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def Mediator(request):
+    template = loader.get_template('Mediator.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def Expert(request):
+    template = loader.get_template('Expert.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def Investigator(request):
+    template = loader.get_template('Investigator.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def eLearning(request):
+    template = loader.get_template('eLearning.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
