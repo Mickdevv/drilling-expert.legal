07c681094505f4e0abd96940b41aa14455a99710
Mickdevv <michael@devereux.fm>
2023-11-14 18:03:49 +0100

    Before python manage.py collectstatic

--- Drilling_expert_legal/Drilling_expert_legal/settings.py
+++ Drilling_expert_legal/Drilling_expert_legal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,15 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '176.58.116.127:8001/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATICFILES_DIRS = [
-#     BASE_DIR / 'main/static',
+#     BASE_DIR / "static",
+#     "http://176.58.116.127:8001/var/www/static",
+#     "/var/www/static",
 # ]
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "http://176.58.116.127:8001/var/www/Drilling_expert_legal/main/static",
-    "/var/www/Drilling_expert_legal/main/static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
