d105bef784e4a727d7ad5957f14573063a04973f
Mickdevv <michael@devereux.fm>
2023-11-15 11:36:25 +0100

    more static tests

--- Drilling_expert_legal/Drilling_expert_legal/settings.py
+++ Drilling_expert_legal/Drilling_expert_legal/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'http://176.58.116.127:8000/'
-STATIC_ROOT = "/static"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "/main/static")
 # ]
